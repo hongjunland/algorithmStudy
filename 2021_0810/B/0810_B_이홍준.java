9af9cf1e99e6bdba3ee1da9c6cbf0e8572b50e8d
이홍준 <zunza96@naver.com>
2021-08-04 10:36:20 +0900

    test

--- 2021_0810/B/0810_B_이홍준.java
+++ 2021_0810/B/0810_B_이홍준.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class 0810_B_이홍준 {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
